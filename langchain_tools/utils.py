c16c8f7a6abec6a00a2704361a191bf0052eb7e8
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-07-02 17:38:21 +0000

    finishing up langchain funcs

--- langchain_tools/utils.py
+++ langchain_tools/utils.py
@@ -1,42 +1,25 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.vectorstores import InMemoryVectorStore
-# from langchain_text_splitters import RecursiveCharacterTextSplitter
-# from pypdf import PdfReader
-# import os
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def create_llm(model_name: str):
     """Create the llm model"""
     return ChatOpenAI(model=model_name)
 
-def create_vector_store(embeddings):
+def load_documents(path: str):
+    return PyPDFDirectoryLoader(path).load()
+
+def create_vector_store(collection_name: str, embeddings):
     """Create the vector store"""
-    return InMemoryVectorStore(embeddings)
+    return Chroma(
+    collection_name=collection_name,
+    embedding_function=embeddings
+)
 
-# def read_document(document: str):
-#     """Extracts text from a pdf file"""
-#     try:
-#         reader = PdfReader(document)
-#         extracted_text = []
-#         N = len(reader.pages)
-#         for page_num in range(N):
-#             page = reader.pages[page_num]
-#             extracted_text.append(page.extract_text())
-#         return "\n".join(extracted_text)
-#     except Exception as e:
-#         print(f"Error reading PDF: {e}")
-#         return None
-    
-# def compose_documents(path: str):
-#     """Returns a list of extracted documents"""
-#     documents = os.listdir(path)
-#     return [
-#         read_document(os.path.join(path, document))
-#         for document in documents
-#     ]
-
-# def split_text(docs, chunk_size: int, overlap: int):
-#     """Splits documents by specific chunk size and overlap"""
-#     splitter = RecursiveCharacterTextSplitter(
-#         chunk_size=chunk_size, chunk_overlap=overlap
-#     )
-#     return splitter.split_documents(docs)
+def split_text(docs, chunk_size: int, overlap: int):
+    """Splits documents by specific chunk size and overlap"""
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=overlap
+    )
+    return splitter.split_documents(docs)
--- langchain_tools/vectors.py
+++ langchain_tools/vectors.py
@@ -1,23 +1,29 @@
-from utils import create_vector_store
+from utils import load_documents, create_vector_store, split_text
 from langchain_core.tools import tool
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 PATH = "../documents/"
 EMBEDDING_MODEL = "text-embedding-3-large"
+CHUNK_SIZE = 1000
+OVERLAP = 200
+COLLECTION_NAME = 'baseball_info'
 
-loader = PyPDFDirectoryLoader(PATH)
-docs = loader.load()
+docs = load_documents(PATH)
 embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-vector_store = create_vector_store(embeddings)
-vector_store.add_documents(docs)
+vector_store = create_vector_store(
+    COLLECTION_NAME, embeddings
+)
+splits = split_text(docs, 1000, 200)
+vector_store.add_documents(splits)
 
-# @tool(response_format="content_and_artifact")
-# def retrieve(query: str, k: int=2) -> tuple:
-#     """Retrieve information related to query."""
-#     retrieved_docs = vectors_store.similarity_search(query, k)
-#     serialized = "\n\n".join(
-#         (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
-#         for doc in retrieved_docs
-#     )
-#     return serialized, retrieved_docs
+@tool(response_format="content_and_artifact")
+def retrieve(query: str, k: int=2) -> tuple:
+    """Retrieve information related to query."""
+    retrieved_docs = vector_store.similarity_search(query, k)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
