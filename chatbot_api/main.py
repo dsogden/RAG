8520b5a2d557ef17ee53e68dce21992dff884169
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-07-07 14:58:19 +0000

    endpoint works and removed some whitespace from files

--- chatbot_api/main.py
+++ chatbot_api/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-
 from langchain_utils.chatbot import run_chatbot
 
 app = FastAPI(
@@ -7,8 +6,7 @@ app = FastAPI(
     description="Endpoint for a general baseball system graph RAG chatbot"
 )
 
-query = "What is the grip for a 4 seam fastball?"
-response = run_chatbot(query)
 @app.get("/")
-async def run():
+async def run(query: str):
+    response = run_chatbot(query)
     return {"response": response["messages"][-1].content}
