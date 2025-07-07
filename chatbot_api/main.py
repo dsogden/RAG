99ab999b04a89eae3e4a9e243ade93b3c028ad7b
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-07-07 18:56:40 +0000

    random changes

--- chatbot_api/main.py
+++ chatbot_api/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from langchain_utils.chatbot import run_chatbot
+from pydantic import BaseModel
 
 app = FastAPI(
     title="Baseball chatbot",
     description="Endpoint for a general baseball system graph RAG chatbot"
 )
 
+query = "How many innings are in baseball?"
+response = run_chatbot(query)
+
+class Response(BaseModel):
+    response: str
+
 @app.get("/")
-async def run(query: str):
-    response = run_chatbot(query)
-    return {"response": response["messages"][-1].content}
+async def run() -> Response:
+    output = response["messages"][-1].content
+    return {"response": output}
