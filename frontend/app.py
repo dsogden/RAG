662febc4ddf3b862a6368f272950d122400650e4
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-07-07 19:22:39 +0000

    back to fixing the frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,28 +1,32 @@
 import streamlit as st
-from langchain_utils.chatbot import run_chatbot
 
 st.title("Chat Model with RAG")
 st.session_state.messages = []
 
 def main():
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    pass
+    # for message in st.session_state.messages:
+    #     with st.chat_message(message["role"]):
+    #         st.markdown(message["content"])
 
-    if prompt := st.chat_input("Ask anything"):
-        st.session_state.messages.append(
-            {"role": "user", "content": prompt}
-        )
-        with st.chat_message('user'):
-            st.markdown(prompt)
-            input_message = prompt
+    # if prompt := st.chat_input("Ask anything"):
+    #     st.session_state.messages.append(
+    #         {"role": "user", "content": prompt}
+    #     )
+    #     with st.chat_message('user'):
+    #         st.markdown(prompt)
+    #         input_message = prompt
 
-        with st.chat_message('assistant'):
-            output = run_chatbot(input_message)
-            response = output['messages'][-1].content
+    #     with st.chat_message('assistant'):
+    #         output = run_chatbot(input_message)
+    #         response = output['messages'][-1].content
             
-            st.markdown(response)
+    #         st.markdown(response)
 
-        st.session_state.messages.append(
-            {"role": "assistant", "content": response}
-        )
+    #     st.session_state.messages.append(
+    #         {"role": "assistant", "content": response}
+    #     )
+
+
+if __name__ == "__main__":
+    main()
